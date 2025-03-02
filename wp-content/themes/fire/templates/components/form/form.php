8723718ff7dceb42780d4f6bb7d86fdcf882bdb2
John Young <theroad2eldorado@gmail.com>
2025-03-01 21:58:17 -0500

    feat: add image field to form component and display responsive image if provided

--- wp-content/themes/fire/templates/components/form/form.php
+++ wp-content/themes/fire/templates/components/form/form.php
@@ -1,6 +1,7 @@
 <?php
   $form = get_sub_field('form');
   $copy = get_sub_field('copy');
+  $image = get_sub_field('image');
 
   $background_texture = get_sub_field('background_texture');
   $background_color = get_sub_field('background_colors');
@@ -19,6 +20,11 @@
     <?php if ($copy) : ?>
       <div class="wizzy col-[main] md:col-[col-2/col-5] mb-10">
         <?php echo $copy; ?>
+        <?php if ($image) : ?>
+          <div class="mt-4 lg:block hidden overflow-hidden rounded-xl lg:rounded-[30px]">
+            <?php echo ResponsivePics::get_picture($image, 'sm:600, md:500, lg:800', 'lazyload-effect full-image', false, false); ?>
+          </div>
+        <?php endif; ?>
       </div>
     <?php endif; ?>
     <?php if ($form) : ?>
